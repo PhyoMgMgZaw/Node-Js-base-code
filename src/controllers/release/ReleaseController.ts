e840583ed3f482981a1f9008f82e2da823a0f76c
Phyo Mg Mg Zaw <zphyomgmg@gmail.com>
2025-11-18 16:14:59 +0630

    base_controller_update

--- src/controllers/release/ReleaseController.ts
+++ src/controllers/release/ReleaseController.ts
@@ -1,10 +1,15 @@
 
 import { BaseController } from "../BaseController";
 import { ReleaseService } from "../../services/releaseService";
+import { createReleaseSchema } from "../../schema/releaseSchema";
 
 export const ReleaseController = new BaseController({
   service: ReleaseService,
   idParamKey: "releaseId",
   resourceName: "release",
   defaultOrderBy: { createdAt: "desc" },
+  defaultInclude: {
+    project: true, // related models can be included here
+  },
+   schema: createReleaseSchema, 
 });
